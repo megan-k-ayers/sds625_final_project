9081704f6e915dbac5fe8ed00d3bc2b9f885b375
mka <m.ayers@yale.edu>
2021-12-01 18:31:08 -0500

    Started final report writeup

---
+++ writeups/Final_Report.R
@@ -0,0 +1,90 @@
+
+#' ---
+#' title: "Exploring the relationship between climate attitudes and extreme weather events - S&DS 625 Final Project"
+#' date: "`r Sys.Date()`"
+#' author: "Megan Ayers"
+#' output: pdf_document
+#' ---
+
+#' # Introduction
+#' \subsection{Contextualizing and motivating}
+#' Climate change and environmental degradation is one of the most existential
+#' crises of our time, if not the most existential. Unfortunately, many
+#' Americans do not agree with this statement and/or do not fully comprehend the
+#' implications of our global future if we do not address the causes of
+#' climate change. Without recognition of the seriousness of climate change,
+#' harmful consumer habits perpetuate, eco-friendly policies falter due to lack
+#' of support, and the industries and corporations that negatively impact the
+#' environment are not held accountable nor pressured to abandon the status quo.
+#' Climate communication is a field that works with these issues, and in
+#' particular, with how to educate and persuade individuals about climate
+#' change and the necessity of acting to reduce it. The hope is that
+#' the more people there are that recognize climate change as a serious threat,
+#' the more likely we are to have politicians and corporations that are
+#' pressured to use their positions of power to enact positive change when it
+#' comes to the environment. 
+#' 
+#' This contextualizes my goal for this final project - to better understand
+#' some aspects of climate change beliefs at the county-level. If we don't 
+#' understand the beliefs that people currently have about climate change,
+#' and what factors might influence those beliefs, then we will be less
+#' effective at trying to persuade those people than if we can be understanding
+#' and empathetic at the same time as being convincing. In particular I am
+#' curious to see how local extreme weather events relate to county-level
+#' climate change beliefs. I would expect that personally experiencing one of
+#' the widely broadcasted affects of climate change - increased extreme weather
+#' events such as drought, hurricanes, wildfires, winter storms, etc - would
+#' cause a person to take climate change more seriously, compared to someone
+#' who just hears about these things happening on the news. However, there are
+#' other cultural and demographic factors that I would expect to have an effect
+#' on climate change opinions that also need to be controlled for, such as
+#' political leanings and age.
+#' 
+#' 
+#' \subsection{Data sources}
+#' To explore the relationship between extreme weather events and climate
+#' change opinion with consideration given to the additional impact of
+#' political leanings and demographics on climate change beliefs, I gathered
+#' information from multiple data sources. To attempt to understand climate
+#' change beliefs, which I will consider to be the response variable, I
+#' will use data collected and processed by the Yale Program on Climate Change
+#' Communication. This data consists of US county-level estimates of climate
+#' change beliefs, i.e. x% of the population in New Haven County believes that
+#' climate change is happening. These estimates are calculated from a
+#' large-scale nation-wide survey (n > 25,000) conducted in 2020, and were
+#' "derived from a statistical model using multilevel regression with
+#' post-stratification (MRP)" While individual-level data may be more powerful
+#' for arguing existence of a causal relationship between extreme weather
+#' event experiences and climate change beliefs, actually obtaining the
+#' supplementary data at this level was not feasible for this project due to
+#' data privacy issues and the fact that it is difficult to guarantee that an
+#' individual experienced an extreme weather event, even if they are recorded
+#' as living in the county where one occurred.
+#' 
+#' Note that individual level data may be preferable but I just have county
+#' level for now.
+#' 
+#' 
+
+
+#' # Methods
+#' 
+
+
+#' # Results
+#' 
+
+
+#' # Conclusion
+#' 
+
+
+#' # References
+#' 
+#' - https://www.theguardian.com/environment/ng-interactive/2021/oct/14/climate-change-happening-now-stats-graphs-maps-cop26
+#' - https://climate-xchange.org/communicating-the-climate-crisis/
+#' - https://climatecommunication.yale.edu/
+#' - https://climatecommunication.yale.edu/visualizations-data/ycom-us/
+#' 
+
+
